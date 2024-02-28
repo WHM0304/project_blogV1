b7211d3264cade18c969626d03fc7ecb0db3e96c
WHM0304 <whm0304@naver.com>
2024-02-28 16:42:09 +0900

    add

--- project_blog/routes/index.js
+++ project_blog/routes/index.js
@@ -1,9 +1,13 @@
 import express from "express";
+import DB from "../models/index.js";
+const NOTICE = DB.models.tbl_notice;
+
 const router = express.Router();
 
 /* GET home page. */
 router.get("/", async (req, res, next) => {
-  res.render("index");
+  const data = await NOTICE.findAll();
+  return res.render("index", { data });
 });
 
 export default router;
--- project_blog/routes/notice.js
+++ project_blog/routes/notice.js
@@ -1,13 +1,27 @@
 import express from "express";
-
+import DB from "../models/index.js";
 const router = express.Router();
+const NOTICE = DB.models.tbl_notice;
 
-router.get("/insert", (req, res) => {
-  return res.render("notice/insert");
+router.get("/insert", async (req, res) => {
+  const data = await NOTICE.findAll();
+  return res.render("notice/insert", { data });
+});
+router.post("/insert", async (req, res) => {
+  const data = req.body;
+  req.body.n_uid = "1";
+  // return res.json(data);
+  try {
+    await NOTICE.create(data);
+  } catch (error) {
+    return res.json(error);
+  }
+  return res.redirect("/");
 });
 
-router.get("/setting", (req, res) => {
-  return res.render("notice/insert");
+router.get("/setting", async (req, res) => {
+  const data = await NOTICE.findAll();
+  return res.render("notice/setting", { data });
 });
 
 export default router;
