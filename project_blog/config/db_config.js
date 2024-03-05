9f1e06a13c938d9ffe60a254f297f69f5ce7f63c
WHM0304 <whm0304@naver.com>
2024-03-05 20:50:17 +0900

    detail

--- project_blog/config/db_config.js
+++ project_blog/config/db_config.js
@@ -1,7 +1,7 @@
 export default {
   development: {
     username: "root",
-    password: "Dga06184##",
+    password: "!Biz8080",
     database: "blogDB",
     host: "127.0.0.1",
     dialect: "mysql",
